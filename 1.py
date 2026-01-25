49d5cc49db22ee1b5b75e0e36ec2a1344f28a5b1
sasinomalabs <sasi@nomasec-labs.com>
2026-01-26 00:57:05 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,9 @@
-print('hello world')
+import socket
+
+s = socket.socket()
+s.connect(("nomasec-labs.ngrok.app", 5000))
+s.sendall(b"ping")
+print(s.recv(4096))
+s.close()
+
+print('hello <b>world</b>')
